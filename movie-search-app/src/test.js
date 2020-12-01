3fdf5850909d2282a95020316eaaad71d5e54c34
Miguel Palo <rmd.palo@gmail.com>
2020-11-30 16:53:56 -0800

    Title search bar and API set up.

---
+++ movie-search-app/src/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Test() {
+  return <h1>Hello World</h1>;
+}
+
+//create SearchMovies component
+//form with a class of form
+//label with htmlFor="query"
+//input of type text with a name of "query" and a placeholder
+//button with class "button" and a type of submit.
